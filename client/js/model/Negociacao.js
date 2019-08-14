353b2c34f86359ee08ce920333b8140bb3082391
Anderson Gaitolini <gaitolini@gmail.com>
2019-08-14 00:00:36 -0300

    start do curso alura avancado
    POO com JS

---
+++ client/js/model/Negociacao.js
@@ -0,0 +1,25 @@
+class Negociacao {
+
+    constructor() {
+        this._data = new Date(data.getTime());
+        this._quantidade = 1;
+        this._valor = 0.0;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+}
